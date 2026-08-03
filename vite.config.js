0994338a0e3dfe9fc306bb18ca08bf12e608bcc7
Malz53 <johnmalunga53@gmail.com>
2026-08-04 00:53:50 +0200

    setting up file structure

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from "url";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '#components': resolve(fileURLToPath(import.meta.url)), 'components'),
+      '#constants': resolve(fileURLToPath(import.meta.url)), 'constants'),
+      '#store': resolve(fileURLToPath(import.meta.url)), 'store'),
+      '#hoc': resolve(fileURLToPath(import.meta.url)), 'hoc'),
+      '#windows': resolve(fileURLToPath(import.meta.url)), 'windows'),
+    }
+  }
 });
