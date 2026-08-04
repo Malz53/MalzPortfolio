e650733ff30a29068357fe64c63d8209bc42cd91
Malz53 <johnmalunga53@gmail.com>
2026-08-05 00:50:43 +0200

    setting up file structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
+const data = [
+        {id: 1, name: "Portfolio"},
+{id: 2, name: "Contact"},
+{id: 3, name: "Projects"},
+]
+
 const Navbar = () => {
     return <nav>
         <div>
-            <img src={"/images/logo.svg"}/>
+            <img src="/images/logo.svg" alt="logo" />
+            <p className="font-bold">Mbakisi's Portfolio</p>
+            <ul>
+                {data.map(({id, name}) => (
+                    <li key={id}>
+                        <p>{name}</p>
+                    </li>
+                ))}
+            </ul>
         </div>
     </nav>
 
